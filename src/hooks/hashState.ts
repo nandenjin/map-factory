e75ddc7307e8ed912811e1adacafa3450f7c76bc
Kazumi Inada <hello@nandenjin.com>
2024-02-05 19:37:02 +0900

    fix: Stop unexpected overwrites of hash params

--- src/hooks/hashState.ts
+++ src/hooks/hashState.ts
@@ -1,20 +1,28 @@
 import { LatLngBounds, LatLngBoundsLiteral, LatLngTuple } from 'leaflet'
 import { useHash } from 'react-use'
 
+/** Get and parse hash parameters */
+const getHash = () =>
+  new URLSearchParams(location.hash.replace(/^#/, '').replace(/^[a-z]*!/, ''))
+
+/** Set hash parameters */
+const setHash = (hash: string) => (location.hash = '#' + hash.replace(/^#/, ''))
+
 /** Hook to get viewId from URL hash state
  *
  * e.g. #mapId!param1=value1&param2=value2
  */
 export function useViewId(): [string, (viewId: string) => void] {
-  const [hash, setHash] = useHash()
+  useHash() // Only for update detection
 
+  const hash = location.hash
   const hashRegex = /^#([a-z_]+)!/
   const test = hash.match(hashRegex)
   const viewId = test ? test[1] : 'default'
 
   const setViewId = (viewId: string) => {
-    const hashParamString = hash.replace(hashRegex, '').replace(/^#/, '')
-    setHash(`#${viewId}!${hashParamString}`)
+    const q = getHash()
+    setHash(`${viewId}!${q.toString()}`)
   }
 
   return [viewId, setViewId]
@@ -22,9 +30,9 @@ export function useViewId(): [string, (viewId: string) => void] {
 
 /** Hook to get center point of map from URL hash state */
 export function useMapCenter(): [LatLngTuple, (center: LatLngTuple) => void] {
-  // URL hash state
-  const [hash, setHash] = useHash()
-  const q = new URLSearchParams(hash.slice(1)) // Remove leading '#'
+  useHash() // Only for update detection
+
+  const q = getHash()
 
   // Map
   const mapCenter: LatLngTuple = (q
@@ -33,8 +41,9 @@ export function useMapCenter(): [LatLngTuple, (center: LatLngTuple) => void] {
     .map(parseFloat) as LatLngTuple) ?? [36.081771, 140.113755]
 
   const setMapCenter = (center: LatLngTuple) => {
+    const q = getHash()
     q.set('center', center.join(','))
-    setHash('#' + q.toString())
+    setHash(q.toString())
   }
 
   return [mapCenter, setMapCenter]
@@ -42,13 +51,15 @@ export function useMapCenter(): [LatLngTuple, (center: LatLngTuple) => void] {
 
 /** Hook to get zoom level of map from URL hash state */
 export function useMapZoom(): [number, (zoom: number) => void] {
-  const [hash, setHash] = useHash()
-  const q = new URLSearchParams(hash.slice(1)) // Remove leading '#'
+  useHash() // Only for update detection
+
+  const q = getHash()
   const mapZoom = +(q.get('zoom') ?? 16)
 
   const setMapZoom = (zoom: number) => {
+    const q = getHash()
     q.set('zoom', zoom.toString())
-    setHash('#' + q.toString())
+    setHash(q.toString())
   }
 
   return [mapZoom, setMapZoom]
@@ -59,8 +70,9 @@ export function useMapBoundsToCapture(): [
   LatLngBoundsLiteral,
   (bounds: LatLngBounds) => void,
 ] {
-  const [hash, setHash] = useHash()
-  const q = new URLSearchParams(hash.slice(1)) // Remove leading '#'
+  useHash() // Only for update detection
+
+  const q = getHash()
 
   const boundCoordsFromHash = q.get('bounds')?.split(',').map(parseFloat)
   const mapBoundsToCapture = (
@@ -78,7 +90,7 @@ export function useMapBoundsToCapture(): [
       [bounds.getSouth(), bounds.getEast()],
     ]
     q.set('bounds', boundsLiteral.map((latlng) => latlng.join(',')).join(','))
-    setHash('#' + q.toString())
+    setHash(q.toString())
   }
 
   return [mapBoundsToCapture, setMapBoundsToCapture]
